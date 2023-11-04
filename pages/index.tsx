5e6de15f4407b7df98cfde9ee0758c7ac7838ec6
Mahmood Sagharjooghi <mahmood.sagharjooghi@oneflow.com>
2023-11-04 18:57:56 +0100

    add basics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
+import Layout from '../components/layout/layout';
 
 const IndexPage = () => (
-  <div>hello</div>
+  <Layout>
+    <div className='main-grid'>
+      <div></div>
+        <article>
+          <h1 className='mb-7'>Mahmood Sagharjooghi</h1>
+          <p className='mb-7'>
+            <i>Web craftsman</i>
+            . Creating delightful and smooth web experiences for users.
+            Frontend dev at <a className='underline' target='_blank' href='https://www.Oneflow.com/'>Oneflow</a>.
+          </p>
+        </article>
+      <div></div>
+    </div>
+  </Layout>
 )
 
 export default IndexPage
