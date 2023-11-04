4a0dfc5da2ec0c39264d98188d71b11303a36744
Mahmood Sagharjooghi <mahmood.sagharjooghi@oneflow.com>
2023-11-04 18:10:13 +0100

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <div>hello</div>
 )
 
 export default IndexPage
