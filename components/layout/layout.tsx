af887a1c32a45a035256c766ed33045e875dd32c
Mahmood Sagharjooghi <32932044+mahmood-saghrajooghi@users.noreply.github.com>
2024-06-30 00:23:51 +0200

    change footer

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout ({ children }) {
       </main>
       <footer>
         <div className='flex justify-between items-center text-sm mx-auto h-full max-w-[640px] color-sub'>
-          <div>Be the GOAT.</div>
+          <div>Be the goat.</div>
           <div>2024</div>
         </div>
       </footer>
